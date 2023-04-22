8e983f89d8d849786cda8ccc72eaad0a0992dbcb
Detective <=>
2023-04-22 10:39:37 +0530

    Added Camera Class

--- OpenGL/src/camera_s.cpp
+++ OpenGL/src/camera_s.cpp
@@ -1,25 +1,4 @@
-/*
 #include <camera_s.h>
-
-Camera::Camera(glm::vec3 position, glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH)
-	: Front(glm::vec3(0.0f, 1.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
-{
-	Position = position;
-	WorldUp = up;
-	Yaw = yaw;
-	Pitch = pitch;
-	updateCameraVectors();
-}
-
-Camera::Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch)
-	: Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
-{
-	Position = glm::vec3(posX, posY, posZ);
-	WorldUp = glm::vec3(upX, upY, upZ);
-	Yaw = yaw;
-	Pitch = pitch;
-	updateCameraVectors();
-}
 
 glm::mat4 Camera::GetViewMatrix()
 {
@@ -79,4 +58,3 @@ void Camera::updateCameraVectors()
 	Right = glm::normalize(glm::cross(Front, WorldUp));
 	Up = glm::normalize(glm::cross(Right, Front));
 }
-*/
